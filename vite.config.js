3d50e695e81273451f1c7c09ca6162baa8cdf9e5
fito zamora <rzamoraamestica@gmail.com>
2025-07-09 00:17:25 -0400

    vfinal

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import sitemap from 'vite-plugin-sitemap';
+// import sitemap from 'vite-plugin-sitemap';
 
 // Configura tu dominio aquí
-const domain = 'https://inmobiliarialauken.cl';  // Cambia a tu dominio real
+// const domain = 'https://inmobiliarialauken.cl';  // Cambia a tu dominio real
 
 export default defineConfig({
   plugins: [
     react(),
-    sitemap({
-      hostname: domain,
-      routes: [
-        '/',
-        '/entre-valles',
-        '/costa-pulin',
-        '/san-rafael',
-      ],
-    }),
+    // sitemap({
+    //   hostname: domain,
+    //   routes: [
+    //     '/',
+    //     '/entre-valles',
+    //     '/costa-pulin',
+    //     '/san-rafael',
+    //   ],
+    // }),
   ],
 });
