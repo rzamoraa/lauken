c51b02a9792b7693c719e9b45e611457c27a2200
Benjamin <mel221919@gmail.com>
2026-03-27 23:04:58 -0300

    Se agregaron imagenes en el footer

--- src/components/sections/ImageShowcase.jsx
+++ src/components/sections/ImageShowcase.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import p1 from '../../assets/images/p1.png';
+import p1 from '../../assets/images/p1.jpg';
 import p2 from '../../assets/images/p2.jpg';
 import p3 from '../../assets/images/p3.jpg';
 
