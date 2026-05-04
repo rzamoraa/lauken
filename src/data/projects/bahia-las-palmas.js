a534595a9748b5fbdca106d0726deb1c1a52e50c
Benjamin <mel221919@gmail.com>
2026-05-04 15:28:03 -0400

    Arreglo de atributos IMG las palmas

--- src/data/projects/bahia-las-palmas.js
+++ src/data/projects/bahia-las-palmas.js
@@ -14,7 +14,7 @@ import g7 from '../../assets/projects/palmas/galeria/foto7.jpg';
 import g8 from '../../assets/projects/palmas/galeria/foto8.jpg';
 import g9 from '../../assets/projects/palmas/galeria/foto9.jpg';
 import g10 from '../../assets/projects/palmas/galeria/foto10.png';
-import atributosImg from '../../assets/projects/palmas/galeria/foto6.jpg'; 
+import atributosImg from '../../assets/projects/palmas/galeria/atributos.jpg'; 
 import mocap from '../../assets/projects/palmas/mockupLP.png';
 import fondo from '../../assets/projects/palmas/bgmocap.jpg';
 
