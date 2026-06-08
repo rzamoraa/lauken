2f48448c66b32d7526e50057aa7297382bccab6e
Benjamin <mel221919@gmail.com>
2026-06-08 11:15:16 -0400

    Se actualizó brochure y mockup de celular en valles de rangue 2

--- src/data/projects/valles-de-rangue-2.js
+++ src/data/projects/valles-de-rangue-2.js
@@ -14,7 +14,7 @@ import g10 from '../../assets/projects/valles-rangue-2/galeria/10.jpg';
 import g11 from '../../assets/projects/valles-rangue-2/galeria/11.jpg';
 import g12 from '../../assets/projects/valles-rangue-2/galeria/12.jpg';
 import atributosImg from '../../assets/projects/valles-rangue-2/atributosAculeo.png';
-import mocap from '../../assets/projects/valles-rangue-2/mockupRangue.png';
+import mocap from '../../assets/projects/valles-rangue-2/mockupvallesrangue2.png';
 import logoNegro from '../../assets/projects/valles-rangue-2/valles de rangue 2 negro logo.png';
 
 const vallesDeRangue = {
@@ -71,7 +71,7 @@ const vallesDeRangue = {
       enabled: true,
       mocap: mocap,
       fondo: g1,
-      link: 'https://storage.googleapis.com/bucket-launken/brochure/BrochureSR.pdf',
+      link: 'https://storage.googleapis.com/lauken/lauken-web/Brochure/valles-de-rangue-2/BrochureVDR2.pdf',
     },
     
     mapa: {
